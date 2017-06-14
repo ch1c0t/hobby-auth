c5858f0f1e09afb03621fa5f445f450fec320dfe
Anatoly Chernow <chertoly@gmail.com>
2017-06-14 23:57:25 +0300

    Raise an error when user models overshadow each other

--- lib/hobby/auth.rb
+++ lib/hobby/auth.rb
@@ -1,10 +1,12 @@
 module Hobby
   module Auth
     def self.[] *user_models
+      pairs = find_pairs user_models
+
       Module.new do
         define_singleton_method :included do |app|
-          user_models.each do |user_model|
-            app.define_singleton_method short_name_of user_model do |route|
+          pairs.each do |name, user_model|
+            app.define_singleton_method name do |route|
               action = route.action
               route.action = -> do
                 if user = (user_model.find_by_token env['HTTP_AUTHORIZATION'])
@@ -19,12 +21,26 @@ module Hobby
           end
         end
 
-        def self.short_name_of user_model
-          user_model.name.split('::').last.downcase
-        end
-
         attr_reader :user
       end
+    end
+
+    class SameNames < StandardError
+    end
+
+    def self.find_pairs user_models
+      array = user_models.map { |um| [(short_name_of um), um] }
+
+      names = array.map &:first
+      unless names.size == names.uniq.size
+        fail SameNames
+      end
+
+      array.to_h
+    end
+
+    def self.short_name_of user_model
+      user_model.name.split('::').last.downcase
     end
   end
 end # Ends should be optional. Block nesting can be inferred from two-space indent.
